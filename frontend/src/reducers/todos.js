a97bd09bd7e0f2991808bbd0f86f8e3ff658e74e
Glendon Philipp Baculio <gpbaculio@gmail.com>
2018-11-30 01:43:37 -0500

    setup toggle all reducer with optimistic update

--- frontend/src/reducers/todos.js
+++ frontend/src/reducers/todos.js
@@ -2,7 +2,8 @@ import _ from 'lodash'
 import {
   ADD_TODO_BY_USER_SUCCESS,
   FETCH_TODOS_BY_USER_SUCCESS,
-  TOGGLE_TODO_COMPLETE_BY_USER_SUCCESS
+  TOGGLE_TODO_COMPLETE_BY_USER_SUCCESS,
+  TOGGLE_ALL_SUCCESS
 } from "../types";
 
 const initialState = {
@@ -42,15 +43,31 @@ export default (state = initialState, action) => {
           ...state.entities,
           [action.todo._id]: action.todo
         },
-        completeIds: _.map(_.values(state.entities).filter(todo => todo.complete), '_id')
+        completeIds: _.map(
+          _.values({ ...state.entities })
+            .filter(todo => todo.complete),
+          '_id')
+      }
+    case TOGGLE_ALL_SUCCESS:
+      return {
+        ...state,
+        entities: action.entities,
+        completeIds: action.completeIds
       }
     case FETCH_TODOS_BY_USER_SUCCESS:
       return {
         ...state,
-        entities: { ..._.keyBy(action.data.todos, (todo) => todo._id) },
+        entities: {
+          ..._.keyBy(
+            action.data.todos,
+            (todo) => todo._id)
+        },
         ids: _.map(action.data.todos, '_id'),
         count: action.data.count,
-        completeIds: _.map(_.values(action.data.todos).filter(todo => todo.complete), '_id')
+        completeIds: _.map(
+          _.values(action.data.todos)
+            .filter(todo => todo.complete),
+          '_id')
       }
     default:
       return state
