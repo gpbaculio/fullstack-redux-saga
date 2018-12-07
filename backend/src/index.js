0e967b41cd16404ea044fe3ce6c1aa85a0f79cc3
Glendon Philipp Baculio <gpbaculio@gmail.com>
2018-12-07 16:38:22 -0500

    add if  condition:

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,12 +27,16 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-// Serve any static files
-app.use(express.static(path.join(__dirname, '../../frontend/build')));
-// Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
 
 
 app.listen(port, () => console.log(`Running on port ${port}`))
