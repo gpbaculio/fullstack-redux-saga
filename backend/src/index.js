015f4e287f80fc62b68435272ebdd082fc25fd48
Glendon Philipp Baculio <gpbaculio@gmail.com>
2018-12-07 02:06:40 -0500

    setup heroku deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,5 +22,13 @@ app.use('/api/todo', todo)
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, '../../frontend/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
+    });
+}
 
 app.listen(8000, () => console.log('Running on localhost:8000'))
