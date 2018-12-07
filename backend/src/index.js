e57cc8de17b21db4a8fea39efe4d5b57252cece9
Glendon Philipp Baculio <gpbaculio@gmail.com>
2018-12-07 13:23:21 -0500

    change npm to yarn

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,13 +26,13 @@ app.use('/api/todo', todo)
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, '../../frontend/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
-    });
-}
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, '../../frontend/build')));
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/build', 'index.html'));
+});
+
 
 app.listen(port, () => console.log(`Running on port ${port}`))
